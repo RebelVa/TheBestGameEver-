912b00616ac1a1eff8826244e9e05109dec70556
RebelVa <158839734+RebelVa@users.noreply.github.com>
2024-03-29 07:08:30 +0300

    доп8

---
+++ Assets/Skripts/AKApchSP.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AKApchSP : MonoBehaviour
+{
+    public Apch apchPrefab;
+    public float delayMin = 3;
+    public float delayMax = 9;
+    
+    private List<Transform> _spawnerPoints;
+    
+    private Apch _apch;
+
+    private void Start()
+    {
+        _spawnerPoints = new List<Transform>(transform.GetComponentsInChildren<Transform>());
+    }
+
+    private void Update()
+    {
+        if (_apch != null) return;
+        if (IsInvoking()) return;
+
+        Invoke("CreateApch", Random.Range(delayMin, delayMax));
+    }
+
+    private void CreateApch()
+    {
+        _apch = Instantiate(apchPrefab);
+        _apch.transform.position = _spawnerPoints[Random.Range(0, _spawnerPoints.Count)].position;
+    }
+}
---
+++ Assets/Skripts/EnemySP1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySP1 : MonoBehaviour
+{
+    private List<Transform> _spawnerPoints;
+
+    private void Start()
+    {
+        _spawnerPoints = new List<Transform>(transform.GetComponentsInChildren<Transform>());
+    }
+}
